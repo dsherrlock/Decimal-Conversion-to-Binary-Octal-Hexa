e445f9aed14e74025d81fbe1478d407931a5926a
@DanielScheherazadde <154036351+dsherrlock@users.noreply.github.com>
2024-06-03 16:55:31 +0800

    converter.cpp

--- converter.cpp
+++ converter.cpp
@@ -1,62 +1,9 @@
 #include <iostream>
 using namespace std;
 
-string binary(int input ){ 
-    string result, temp;
-    int temp2;
-
-    while (input != 0)
-    {
-    temp2 = input %2;
-    temp = to_string(temp2);
-    input = input / 2;
-    result = temp + result;
-    }
-    return result;
-}
-string octal(int input ){ 
-    string result, temp;
-    int temp2;
-
-    while (input != 0)
-    {
-    
-    temp2 = input %8;
-    temp = to_string(temp2);
-    input = input / 8;
-    result = temp + result;
-    }
-    
-    return result;
-}
-string hexa(int input ){ 
-    string result, temp;
-    int temp2;
-
-    while (input != 0)
-    {
-    
-    temp2 = input %16;
-        if (temp2 == 10)
-            temp = "A";
-        else if (temp2 == 11)
-            temp = "B";
-        else if (temp2 == 12)
-            temp = "C";
-        else if (temp2 == 13)
-            temp = "D";
-        else if (temp2 == 14)
-            temp = "E";
-        else if (temp2 == 15)
-            temp = "F";
-        else
-            temp = to_string(temp2);
-
-    input = input / 16;
-    result = temp + result;
-    }
-    return result;
-}
+string binary(int input);
+string octal(int input);
+string hexa(int input);
 
 int main(){
     int decimal;
@@ -74,4 +21,58 @@ int main(){
     cout << "HexaDecimal   |  " << base16;
 
     return 0;
+}
+string binary(int input ){ 
+    string result, temp;
+    int temp2;
+
+    while (input != 0)
+    {
+        temp2 = input %2;
+        temp = to_string(temp2);
+        input = input / 2;
+        result = temp + result;
+    }
+    return result;
+}
+string octal(int input ){ 
+    string result, temp;
+    int temp2;
+
+    while (input != 0)
+    {
+        temp2 = input %8;
+        temp = to_string(temp2);
+        input = input / 8;
+        result = temp + result;
+    }
+    return result;
+}
+string hexa(int input ){ 
+    string result, temp;
+    int temp2;
+
+    while (input != 0)
+    {
+        temp2 = input %16;
+
+        if (temp2 == 10)
+            temp = "A";
+        else if (temp2 == 11)
+            temp = "B";
+        else if (temp2 == 12)
+            temp = "C";
+        else if (temp2 == 13)
+            temp = "D";
+        else if (temp2 == 14)
+            temp = "E";
+        else if (temp2 == 15)
+            temp = "F";
+        else
+            temp = to_string(temp2);
+
+        input = input / 16;
+        result = temp + result;
+    }
+    return result;
 }
